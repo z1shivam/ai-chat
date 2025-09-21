4f0ff4d15c184ee7b8252f4d2ad220ced97a753c
z1shivam <shivamk.json@gmail.com>
2025-09-21 12:02:25 +0530

    setup ai components

---
+++ src/components/ui/custom/ai_input.tsx
@@ -0,0 +1,78 @@
+"use client";
+import {
+  PromptInput,
+  PromptInputButton,
+  PromptInputModelSelect,
+  PromptInputModelSelectContent,
+  PromptInputModelSelectItem,
+  PromptInputModelSelectTrigger,
+  PromptInputModelSelectValue,
+  PromptInputSubmit,
+  PromptInputTextarea,
+  PromptInputToolbar,
+  PromptInputTools,
+} from "@/components/ui/shadcn-io/ai/prompt-input";
+import { MicIcon, PaperclipIcon } from "lucide-react";
+import { type FormEventHandler, useState } from "react";
+const models = [
+  { id: "gpt-4o", name: "GPT-4o" },
+  { id: "claude-3-5-sonnet-20241022", name: "Claude 3.5 Sonnet" },
+  { id: "gemini-1.5-pro", name: "Gemini 1.5 Pro" },
+];
+const AiInput = () => {
+  const [text, setText] = useState<string>("");
+  const [model, setModel] = useState<string>(models[0]!.id);
+  const [status, setStatus] = useState<
+    "submitted" | "streaming" | "ready" | "error"
+  >("ready");
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    if (!text) {
+      return;
+    }
+    setStatus("submitted");
+    setTimeout(() => {
+      setStatus("streaming");
+    }, 200);
+    setTimeout(() => {
+      setStatus("ready");
+      setText("");
+    }, 2000);
+  };
+  return (
+    <div className="w-full p-8">
+      <PromptInput onSubmit={handleSubmit}>
+        <PromptInputTextarea
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          placeholder="Type your message..."
+        />
+        <PromptInputToolbar>
+          <PromptInputTools>
+            <PromptInputButton>
+              <PaperclipIcon size={16} />
+            </PromptInputButton>
+            {/* <PromptInputButton>
+              <MicIcon size={16} />
+              <span>Voice</span>
+            </PromptInputButton> */}
+            <PromptInputModelSelect onValueChange={setModel} value={model}>
+              <PromptInputModelSelectTrigger>
+                <PromptInputModelSelectValue />
+              </PromptInputModelSelectTrigger>
+              <PromptInputModelSelectContent>
+                {models.map((model) => (
+                  <PromptInputModelSelectItem key={model.id} value={model.id}>
+                    {model.name}
+                  </PromptInputModelSelectItem>
+                ))}
+              </PromptInputModelSelectContent>
+            </PromptInputModelSelect>
+          </PromptInputTools>
+          <PromptInputSubmit disabled={!text} status={status} />
+        </PromptInputToolbar>
+      </PromptInput>
+    </div>
+  );
+};
+export default AiInput;
