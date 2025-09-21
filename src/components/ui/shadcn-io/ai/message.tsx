688bb8c32a78ea18cf248ec752dd62aa422d9385
z1shivam <shivamk.json@gmail.com>
2025-09-21 12:15:15 +0530

    fixed classnames

--- src/components/ui/shadcn-io/ai/message.tsx
+++ src/components/ui/shadcn-io/ai/message.tsx
@@ -55,7 +55,7 @@ export const MessageAvatar = ({
   ...props
 }: MessageAvatarProps) => (
   <Avatar
-    className={cn('size-8 ring ring-1 ring-border', className)}
+    className={cn('size-8 ring ring-border', className)}
     {...props}
   >
     <AvatarImage alt="" className="mt-0 mb-0" src={src} />
